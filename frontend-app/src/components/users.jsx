13d46bfc438772ec46d94a59d27a68cd8987bb0e
Maciej Grzyb <mgrzyb@sii.pl>
2018-08-21 21:29:07 +0200

    added showing number of users in db

--- frontend-app/src/components/users.jsx
+++ frontend-app/src/components/users.jsx
@@ -3,22 +3,38 @@ import UsersTable from "./usersTable";
 
 class Users extends Component {
   state = {
-    users: [],
+    users: [{ id: 1, username: "abc", email: "abc@pl" }],
     sortColumn: { path: "username", order: "asc" }
   };
+
+  componentDidMount() {
+    const users = [];
+    this.setState(users);
+  }
 
   handleSort = sortColumn => {
     this.setState({ sortColumn });
   };
 
+  handleDelete = user => {
+    console.log(user.id);
+  };
+
   render() {
+    const { length: count } = this.state.users;
+
+    if (count === 0) return <p>There are no users</p>;
+
     return (
-      <UsersTable
-        users={this.state.users}
-        sortColumn={this.state.sortColumn}
-        onDelete={this.handleDelete}
-        onSort={this.handleSort}
-      />
+      <div style={{ marginTop: 20 }}>
+        <p>Showing {count} users in the database.</p>
+        <UsersTable
+          users={this.state.users}
+          sortColumn={this.state.sortColumn}
+          onDelete={this.handleDelete}
+          onSort={this.handleSort}
+        />
+      </div>
     );
   }
 }
