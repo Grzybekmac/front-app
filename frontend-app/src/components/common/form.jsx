5f063074be43653043c2a9e2f2086e64243daa4c
Maciej Grzyb <mgrzyb@sii.pl>
2018-08-24 11:48:02 +0200

    fixed validation

--- frontend-app/src/components/common/form.jsx
+++ frontend-app/src/components/common/form.jsx
@@ -11,7 +11,7 @@ class Form extends Component {
   validate = () => {
     const options = { abortEarly: false };
     const { error } = Joi.validate(this.state.data, this.schema, options);
-    if (!error) return false;
+    if (!error) return null;
 
     const errors = {};
     for (let item of error.details) errors[item.path[0]] = item.message;
